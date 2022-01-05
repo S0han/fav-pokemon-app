64ed025a959a1a0569dd5003a269eef6f6f09ac0
S0han <43890209+S0han@users.noreply.github.com>
2022-01-05 17:18:46 -0500

    updated about page

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -11,6 +11,14 @@ const AboutPage = () => (
              
             I built this purely as aportfolio app to test out my skills with Reactjs and Firebase.
         </h2>
+
+        <h2>
+            This app will let you search and store your favourite pokemon. 
+            
+            If you log out and log in at another time it will display your saved pokemon. 
+
+            You would like to reset hit delete to then search/store a new pokemon.
+        </h2>
     </div>
 );
 
